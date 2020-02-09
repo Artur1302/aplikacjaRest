606621ab7108bed6b6c7f2e1f49d53343f187faf
Artur <artur.osial@gmail.com>
2020-02-09 11:36:53 +0100

    Inicjalizaja projektu testowego

---
+++ src/main/java/com/example/test/DriverRepository.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data  repository for the Driver entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface DriverRepository extends JpaRepository<Driver, Long> {
+
+}
